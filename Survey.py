2734de4dd488e8afa45de51cc19fa42452edc220
RoSiGod <siddhesh4godbole@gmail.com>
2023-09-08 10:26:51 +1000

    8 SEP
    Post discussion with Damien and Prof Tuan

--- Survey.py
+++ Survey.py
@@ -7,6 +7,35 @@ Created on Mon Sep  4 12:39:43 2023
 
 import streamlit as st
 import sqlite3
+import pandas as pd
+
+
+# Define the style class
+class style():
+      
+    h1color = 'darkblue'
+    h2color = 'Black'
+    h3color = 'Black'
+    h4color = 'grey'
+    btextcolor = 'Black'
+    
+    h1size = 20
+    h2size = 20
+    h3size= 16 
+    h4size = 12 
+    btextsize = 16
+
+# Define custom CSS styles for the container
+container_styles = """
+<style>
+    .custom-container {
+        background-color: #f0f0f0; /* Grey background color */
+        padding: 20px; /* Adjust padding as needed */
+        border-radius: 10px; /* Add rounded corners */
+    }
+</style>
+"""
+
 
 # Create or connect to the SQLite database
 conn = sqlite3.connect('survey_responses.db')
@@ -36,12 +65,46 @@ c.execute('''
 ''')
 conn.commit()
 
+
+
+#====================================================================
+# HEADER PAGE CONTENT
+#====================================================================
+col1, colmid, col2 = st.columns([1,5, 1])
+with col1:
+    # Define the URL you want to link to
+    link_url = "https://www.prefabaus.org.au/"
+    st.image('PrefabAUS_logo.png', use_column_width=True)
+    
+with col2:
+    st.image('Unimelb_logo.png', use_column_width=True)
+
 # Streamlit app title
 st.title("Australian Prefabrication Supply Chain Survey")
 st.write('')
 st.write('')
 st.write('')
 st.write('')
+
+
+Q = 'PrefabAUS and the University of Melbourne are collaborating on a comprehensive survey aimed at gaining valuable insights into the prefabricated construction supply chain within Australia. \
+This research initiative seeks to better understand the dynamics, challenges, and opportunities that shape the prefabrication sector.\
+By participating in this survey, you are contributing to a collective effort to enhance the efficiency, sustainability, \
+and innovation within the construction industry. '
+fstring1 = '<p style = "color:' + str(style.h4color) + '; font-size: ' + str(16) + 'px;text-align:left;" > <b>' + str(Q) + '</b> </p>'
+st.markdown(fstring1, unsafe_allow_html=True)
+
+Q = 'Your firsthand knowledge and experiences as a supply chain partner are crucial in helping us identify best practices, \
+areas for improvement, and future trends in prefabricated construction. Your input will not only aid in strengthening the industrys\
+capabilities but also inform policy decisions and educational initiatives. We value your expertise and invite you to share your perspectives and recommendations. \
+Together, we can shape a more resilient, efficient, and sustainable future for construction in Australia.Thank you for your participation in this important survey, which will undoubtedly play a pivotal \
+role in advancing the prefabrication sector in our country.'
+fstring1 = '<p style = "color:' + str(style.h4color) + '; font-size: ' + str(16) + 'px;text-align:left;" > <b>' + str(Q) + '</b> </p>'
+st.markdown(fstring1, unsafe_allow_html=True)
+
+#====================================================================
+# END OF HEADER PAGE CONTENT
+#====================================================================
 
 st.video('https://www.youtube.com/watch?v=akKgTf3cb-4')
 
@@ -57,19 +120,64 @@ st.write('')
 st.write('')
 st.write('')
 # Survey questions and input fields
-q1 = st.text_input("1. What is your Business Name")
+
+c1 = 1
+c2 = 2
+
+col1, col2 = st.columns([c1,c2])
+with col1:
+    # Question string
+    st.write('')
+    Q = 'What is your Business Name?'
+    fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+    st.markdown(fstring1, unsafe_allow_html=True)
+with col2:
+    q1 = st.text_input('',key='q1')
 st.write('')
 
-q2 = st.text_input("2. What is the year in which you business started operations?")
+
+col1, col2 = st.columns([c1,c2])
+with col1:
+    st.write('')
+    Q = 'In which year did the company start its operations?'
+    fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+    st.markdown(fstring1, unsafe_allow_html=True)
+with col2:
+    q2 = st.text_input("", key= 'q2')
 st.write('')
 
-q3 = st.selectbox("3. What is the type of your business closes to the description below?", ("Material processessor", "product manufacuter", 'Wholesale distributor', 'frame fabricator', 'warehouse and logistics facilitator', 'construction company', 'design company', 'engineering services'))
+
+col1, col2 = st.columns([c1,c2])
+with col1:
+    st.write('')
+    Q = 'What best descibes your business from the options below?'
+    fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+    st.markdown(fstring1, unsafe_allow_html=True)
+with col2:
+    q3 = st.selectbox('', ("Material processessor", "product manufacuter", 'Wholesale distributor', 'frame fabricator', 'warehouse and logistics facilitator', 'construction company', 'design company', 'engineering services'))
 st.write('')
 
-q4 = st.selectbox("4. What level of customer value chain do you operate at?",('Engineer to order', 'make to order', 'assemble to order', 'make to stock'))
+
+col1, col2 = st.columns([c1,c2])
+with col1:
+    st.write('')
+    Q = 'What level of customer value chain do you operate at?'
+    fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+    st.markdown(fstring1, unsafe_allow_html=True)
+with col2:
+    q4 = st.selectbox(" ",('Engineer to order', 'make to order', 'assemble to order', 'make to stock'))
 st.write('')
 
-q5 = st.select_slider("5. What proportion of your business is in Prefab?", ('Less than 10%', '10-20%', '20-30%', '30-40%', '40-50%', '50-60%', '60-70%', '70-80%', '80-90%', '90-100%'))
+
+
+col1, col2 = st.columns([c1,c2])
+with col1:
+    st.write('')
+    Q = 'What proportion of your business is in Prefab?'
+    fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+    st.markdown(fstring1, unsafe_allow_html=True)
+with col2:
+    q5 = st.select_slider(" ", ('Less than 10%', '10-20%', '20-30%', '30-40%', '40-50%', '50-60%', '60-70%', '70-80%', '80-90%', '90-100%'))
 
 st.write('')
 st.write('')
@@ -86,31 +194,87 @@ st.write('')
 st.write('')
 st.write('')
 st.write('')
-q6 = st.select_slider("6. What is your approx annual revenue", ('Less than $1M', '$1M-$5M', '$5M-$10M', '$10M-$20M', '$20M-$50M', '$50M-$100M', '$100M-$200M', '$200M-$500M', '$500M-$1B', 'More than $1B'))
+
+
+col1, col2 = st.columns([c1,c2])
+with col1:
+    st.write('')
+    Q = 'What is your approx annual revenue'
+    fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+    st.markdown(fstring1, unsafe_allow_html=True)
+with col2:
+    q6 = st.select_slider(" ", ('Less than $1M', '$1M-$5M', '$5M-$10M', '$10M-$20M', '$20M-$50M', '$50M-$100M', '$100M-$200M', '$200M-$500M', '$500M-$1B', 'More than $1B'))
 st.write('')
 
-q7 = st.multiselect('7. What industry segment do you serve?', ('Residential', 'Commercial/retail', 'Industrial/warehouse/heavy', 'Institutional (Schools, hospitals)', 'Public infrastructure', 'Building services/speciality trades (electrical MEP, HVAC, carpentry, masonry, roofing'))
-st.write('')
+
+
+col1, col2 = st.columns([c1,c2])
+with col1:
+    st.write('')
+    Q = 'What industry segment do you serve?'
+    fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+    st.markdown(fstring1, unsafe_allow_html=True)
+with col2:
+    q7 = st.multiselect('', ('Residential', 'Commercial/retail', 'Industrial/warehouse/heavy', 'Institutional (Schools, hospitals)', 'Public infrastructure', 'Building services/speciality trades (electrical MEP, HVAC, carpentry, masonry, roofing'))
+st.write('');st.write('');st.write('');st.write('')
 q7 = ', '.join(q7)
 
 
-q8 = st.multiselect('8. What type of prefab products do you manufacture/supply?', ('Cat-1: Volumetric modules with internal fitouts', 'Cat-2: 3D Volumetric module skeletons', 'Cat-3: 2D panelised systems with multiplie subsystems',
-                                                                                   'Cat-4: 2D panels without sub-systems', 'Cat-5: Linear elements precut to sizes', 'Cat-6: Processed building materials and equipment'))
-q8 = ', '.join(q8)
+Q = 'What type of prefab products do you manufacture/supply?'
+fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+st.markdown(fstring1, unsafe_allow_html=True)
+
 st.image('product_category.png', use_column_width=True)
 
-st.write('')
+q8 = st.multiselect('', ('Cat-1: Volumetric modules with internal fitouts', 'Cat-2: 3D Volumetric module skeletons', 'Cat-3: 2D panelised systems with multiplie subsystems',
+                                                                                   'Cat-4: 2D panels without sub-systems', 'Cat-5: Linear elements precut to sizes', 'Cat-6: Processed building materials and equipment'))
+q8 = ', '.join(q8)
+st.write('');st.write('');st.write('');st.write('')
 
-q9 = st.text_input("9. What compliances and certifications have you obtained for your products? (Ex., CodeMark, WaterMark, ISO, etc.)")
-st.write('')
 
-q10 = st.multiselect("10. What type of materials do you use in your modular builds? (if applicable)", ('Resilient floor coverings', 'Waterproofing membranes', 'Sanitaryware tiles', 'Epoxies','Levelling compounds','Timber','Light gauge steel',
-                                                                                                       'Structural steel','XPS board','Concrete', 'FRP/GFRP composites'))
+
+col1, col2 = st.columns([c1,c2])
+with col1:
+    st.write('')
+    Q = 'What compliances and certifications have you obtained for your products?'
+    fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+    st.markdown(fstring1, unsafe_allow_html=True)
+with col2:
+    q9 = st.multiselect("9.  (Ex., CodeMark, WaterMark, ISO, etc.)", ('CodeMark', 'WaterMark', 'Australian Made Certification','GECA (Good Environmental Choice Australia)', 
+                                                                                                                                'ISO 9001:2015 (Quality Management Systems)', 'ISO 14001:2015 (Environmental Management Systems)', 'ISO 45001:2018 (Occupational Health and Safety Management Systems)', 'Other'))
+# if other is chosen as one of the options in q9, then offer a text field to insert the other certification
+if 'Other' in q9:
+    q9_other = st.text_input('Enter the other certifications (comma (,) separated)', key='q9_other')
+    q9.append(q9_other)
+q9 = ', '.join(q9)
+st.write('');st.write('');st.write('');st.write('')
+
+
+
+col1, col2 = st.columns([c1,c2])
+with col1:
+    st.write('')
+    Q = 'What type of materials do you use in your modular builds?'
+    fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+    st.markdown(fstring1, unsafe_allow_html=True)
+with col2:
+    q10 = st.multiselect("10.  (if applicable)", ('Resilient floor coverings', 'Waterproofing membranes', 'Sanitaryware tiles', 'Epoxies','Levelling compounds','Timber','Light gauge steel',
+                                                                                                       'Structural steel','XPS board','Concrete', 'FRP/GFRP composites', 'Other'))
+if 'Other' in q10:
+    q10_other = st.text_input('Enter the other materials (comma (,) separated', key='q10_other')
+    q10.append(q10_other)
 q10 = ', '.join(q10)
+st.write('');st.write('');st.write('');st.write('')
 
-st.write('')
 
-q11 = st.select_slider('11. What is the average lead time for a typical order?', ('Instant','<15 days','< 1 month','<2 months','<3 months','<8 months','<1 year'))
+col1, col2 = st.columns([c1,c2])
+with col1:
+    st.write('')
+    Q = 'What is the average lead time for a typical order?'
+    fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+    st.markdown(fstring1, unsafe_allow_html=True)
+with col2:
+    q11 = st.select_slider('', ('Instant','<15 days','< 1 month','<2 months','<3 months','<8 months','<1 year'))
 
 st.write('')
 st.write('')
@@ -128,38 +292,108 @@ st.write('')
 st.write('')
 st.write('')
 
-col1, col2, col3 = st.columns([1,1,1])
+Q = 'Enter your typical throughput capacity in the units of your choice'
+fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+st.markdown(fstring1, unsafe_allow_html=True)
+
+Ex = 'Example: 1000 m3 per day, 1000 kgs per week, 1000 m2 per month, 1000 units per year'
+fstring1 = '<p style = "color:' + str(style.h4color) + '; font-size: ' + str(style.h4size) + 'px;" > <b>' + str(Ex) + '</b> </p>'
+st.markdown(fstring1, unsafe_allow_html=True)
+
+
+st.write('')
+
+col1, col2 = st.columns([c1,c2])
 with col1:
-    q121 = st.number_input('What is the typical maximum through put of your company?')
+    st.write('Enter quantity')
 with col2:
-    q122 = st.text_input('Enter the units of the number')
-with col3:
-    q123 = st.text_input('Enter the time period of the throughput, Ex. per day, per week, per month, per year')
+    q12_1 = st.number_input('',key='q12-qty')
 
-q12 = str(q121) + ' ' + str(q122) + ' per ' + str(q123)
-st.write(q12)
+col1, col2 = st.columns([c1,c2])
+with col1:
+    st.write('Enter units')
+with col2:
+    q12_2 = st.text_input('',key='q12-unt')
 
-st.write('')
-q13 = st.text_input('What product warranties and support do you provide?')
+col1, col2 = st.columns([c1,c2])
+with col1:
+    st.write('Enter period')
+with col2:
+    q12_3 = st.text_input('',key='q12-time')
 
-st.write('')
-q14 = st.multiselect('What are your key value offerings as a business?', ('Reduced environmental impact','Locally sourced materials','Compliance with sustainable and ecofriendly practices','Gender inclusive workforce','Higher robotic automation','Other'))
+q12 = str(q12_1) + ' ' + str(q12_2) + ' per ' + str(q12_3)
+st.write('you have entered:',q12)
+st.write(''); st.write(''); st.write(''); st.write('')
+
+
+
+
+col1, col2 = st.columns([c1,c2])
+with col1:
+    st.write('')
+    Q = 'What product warranties and support do you provide?'
+    fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+    st.markdown(fstring1, unsafe_allow_html=True)
+with col2:
+    q13 = st.multiselect('', ('Statutary warranty','Manufacturer warranty','Design life warranty','Implied warranties (Ex. Australian Consumer Law)','Product specific warranties','Other'))
+if 'Other' in q13:
+    q13_other = st.text_input('Enter the other types of warranties', key='q13_other')
+    q13.append(q13_other)
+q13 = ', '.join(q13)
+st.write(''); st.write(''); st.write(''); st.write('')
+
+
+
+col1, col2 = st.columns([c1,c2])
+with col1:
+    st.write('')
+    Q = 'What are your key value offerings as a business?'
+    fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+    st.markdown(fstring1, unsafe_allow_html=True)
+with col2:
+    q14 = st.multiselect('', ('Reduced environmental impact','Locally sourced materials','Compliance with sustainable and ecofriendly practices','Gender inclusive workforce','Higher robotic automation','Other'))
+if 'Other' in q14:
+    q14_other = st.text_input('Enter the other value offerings', key='q14_other')
+    q14.append(q14_other)
 q14 = ', '.join(q14)
+st.write(''); st.write(''); st.write(''); st.write('')
 
 
-st.write('')
-q15 = st.text_area('What are the key challenges when constructing using prefab in your business?')
 
-# Submit response button
-if st.button("Submit Response"):
-    # Insert the response into the database
 
-    c.execute('''
-        INSERT INTO survey_responses (q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ''', (q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15))
-    conn.commit()
-    st.success("Response submitted successfully. Thank you!")
+
+Q = 'What are the top challenges in your prefab business that PrefabAUS can advocate for on your behalf?'
+fstring1 = '<p style = "color:' + str(style.h3color) + '; font-size: ' + str(style.h3size) + 'px;" > <b>' + str(Q) + '</b> </p>'
+st.markdown(fstring1, unsafe_allow_html=True)
+q15 = st.text_area('')
+st.write(''); st.write(''); st.write(''); st.write('')
+st.write(''); st.write(''); st.write(''); st.write('')
+st.write(''); st.write(''); st.write(''); st.write('')
+
+# Collate all inputs in a list
+survey_inputs = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q12, q13, q14, q15]
+
+
+
+col1, colm, col2 = st.columns([1,3,1])
+with col1:
+    # on clicking Display inputs button, display the dataframe
+    if st.button("Display inputs"):
+        st.write(survey_inputs)
+
+
+
+with colm:
+    # Submit response button
+    if st.button("Submit Response", type = 'primary', use_container_width=True):
+        # Insert the response into the database
+
+        c.execute('''
+            INSERT INTO survey_responses (q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15))
+        conn.commit()
+        st.success("Response submitted successfully. Thank you!")
 
 # Close the database connection
 conn.close()
@@ -176,3 +410,26 @@ st.sidebar.header(num_responses)
 
 
 
+#====================================================================
+# Footer PAGE CONTENT
+#====================================================================
+st.write('');st.write('');st.write('');st.write('');st.write('');st.write('');st.write('');st.write('')
+st.write('');st.write('');st.write('');st.write('');st.write('');st.write('');st.write('');st.write('')
+st.write('');st.write('');st.write('');st.write('');st.write('');st.write('');st.write('');st.write('')
+Q = '2023 Australian prefabrication supply chain survey By PrefabAUS and the University of Melbourne'
+
+
+# Apply custom CSS styles
+st.markdown(container_styles, unsafe_allow_html=True)
+
+# Create a container-like effect with grey background
+st.markdown('<div class="custom-container"> ' + Q + '</div>', unsafe_allow_html=True)
+
+col1, colmid, col2 = st.columns([1,5, 1])
+with col1:
+    # Define the URL you want to link to
+    link_url = "https://www.prefabaus.org.au/"
+    st.image('PrefabAUS_logo.png', use_column_width=True)
+    
+with col2:
+    st.image('Unimelb_logo.png', use_column_width=True)
