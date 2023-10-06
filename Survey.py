6eda1c1a05ac36b14f7fd5987110e2e454653083
RoSiGod <58670691+RoSiGod@users.noreply.github.com>
2023-10-06 16:47:29 +1100

    Update Survey.py

--- Survey.py
+++ Survey.py
@@ -466,7 +466,7 @@ with colm:
     if st.button("Submit Response", type = 'primary', use_container_width=True):
         # Insert the response into the database
         if AllInputsFilled(survey_inputs):
-            query = ''' INSERT INTO''' + str(database_name) + '''(q1, q2, q2_1, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+            query = ''' INSERT INTO ''' + str(database_name) + ''' (q1, q2, q2_1, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
             c.execute(query, (q1, q2, q2_1, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16))
             conn.commit()
             st.success("Response submitted successfully. Thank you!")
